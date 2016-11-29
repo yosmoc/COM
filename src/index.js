afa3e181ba1c207e8f77ce07662ddf74715ad18f
yosmoc <motihara@gmail.com>
2016-11-29 23:05:16 +0100

    implement receive function

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 const SerialPort = require('serialport')
 const port = new SerialPort("/dev/tty.usbserial-FTGXR6TY",
-  {baudrate: 115200}
+  { baudrate: 115200 }
 )
 
 port.on('open', err => {
   if (err) {
     return console.log('Error: ', err.message)
+  } else {
+    port.on('data', data => {
+      console.log('Data received: ' + data)
+    })
+
+    port.write('main screen turn on', err => {
+      if (err) {
+        return console.log('Error on write: ', err.message)
+      }
+    })
   }
-
-  port.write('main screen turn on', err => {
-    if (err) {
-      return console.log('Error on write: ', err.message)
-    }
-
-    console.log('message written')
-  })
 })
