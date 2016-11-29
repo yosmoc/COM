b0fc8a4a0c7167c1bc11c3941a3b258424ab665f
yosmoc <motihara@gmail.com>
2016-11-29 22:39:16 +0100

    write sample application
    succeed to write the message

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("hello world")
+const SerialPort = require('serialport')
+const port = new SerialPort("/dev/tty.usbserial-FTGXR6TY",
+  {baudrate: 115200}
+)
+
+port.on('open', err => {
+  if (err) {
+    return console.log('Error: ', err.message)
+  }
+
+  port.write('main screen turn on', err => {
+    if (err) {
+      return console.log('Error on write: ', err.message)
+    }
+
+    console.log('message written')
+  })
+})
